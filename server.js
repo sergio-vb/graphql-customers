7ca9d2a53514b786464e1451a625a04f1925cd3e
Svb home <sergio.valverde.b@gmail.com>
2017-08-13 14:35:45 -0600

    Adding initial files

---
+++ server.js
@@ -0,0 +1,15 @@
+//Setting up a simple Express server
+
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema.js');
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+    schema: schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
+});
