ab8f1c9fde9c2e1870b17f5723ddea082be8b382
Sergio Valverde <sergio.valverde.b@gmail.com>
2017-08-14 09:53:06 -0600

    Adding graphql query to fetch all customers

--- schema.js
+++ schema.js
@@ -53,11 +53,14 @@ const RootQuery = new GraphQLObjectType({
                 id: {type: GraphQLString}
             },
             resolve(parentValue, args){
-                for(let i = 0; i < customers.length; i++){
-                    if(customers[i].id === args.id){
-                        return customers[i];
-                    }
-                }
+                return customers.filter(x => x.id === args.id)[0];
+            }
+        },
+        customers:{
+            type: new GraphQLList(CustomerType),
+            args: {},
+            resolve(parentValue, args){
+                return customers;
             }
         }
     }
